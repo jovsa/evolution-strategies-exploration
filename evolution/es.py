0d0154e329c6a82bd28565f90852656799cd6a7b
JovanSardinha <jovan.sardinha@gmail.com>
2017-08-03 01:58:22 +0000

    initial version of multi-threading

--- evolution/es.py
+++ evolution/es.py
@@ -1,5 +1,8 @@
 # dependencies
 import numpy as np
+import threading
+from collections import deque
+
 
 class EvolutionStrategy(object):
 
@@ -47,7 +50,8 @@ class EvolutionStrategy(object):
                 A = np.array([p[index] for p in population])
                 self.weights[index] = w + self.LEARNING_RATE/(self.POPULATION_SIZE*self.SIGMA) * np.dot(A.T, rewards).T
     
-    def worker(self):
+    def worker(self, worker_name, return_queue):
+        print('worker:',  worker_name)
         population = []     
         rewards = np.zeros(self.POPULATION_SIZE)
             
@@ -62,7 +66,8 @@ class EvolutionStrategy(object):
             rewards[i]  = self.get_reward(weights_try)
 
         rewards = (rewards - np.mean(rewards)) / np.std(rewards)
-        return population, rewards 
+        return_queue.append([population, rewards])
+ 
     
     def run_dist(self, iterations, print_step=10, num_workers=1):
         for iteration in range(iterations):
@@ -70,17 +75,38 @@ class EvolutionStrategy(object):
             if iteration % print_step == 0:
                 print('iter %d. reward: %f' % (iteration, self.get_reward(self.weights)))
             
-#            for i in range(num_workers):
-#                p = multiprocess.Process(target=self.worker, ar)
-#                
-#                
-#                p1 = multiprocessing.Process(target=calc_square, args=(arr, temp))
             
-            population, rewards = self.worker()
+            return_queue = deque()
             
-            #print('sum of reward', np.sum(rewards))
+            #for worker in num_workers:
+            t1= threading.Thread(target=self.worker, args=(str(1), return_queue))
+            t2= threading.Thread(target=self.worker, args=(str(2), return_queue))
+            t3= threading.Thread(target=self.worker, args=(str(3), return_queue))
             
-            for index, w in enumerate(self.weights):
-                A = np.array([p[index] for p in population])
-                self.weights[index] = w + self.LEARNING_RATE/(self.POPULATION_SIZE*self.SIGMA) * np.dot(A.T, rewards).T
+            t1.start()
+            t2.start()
+            t3.start()
+            
+            t1.join()
+            t2.join()
+            t3.join()
+            
+            #print('work done')
+            for worker_output in return_queue:
+                population = worker_output[0]
+                rewards = worker_output[1]
+                
+                for index, w in enumerate(self.weights):
+                    A = np.array([p[index] for p in population])
+                    self.weights[index] = w + self.LEARNING_RATE/(self.POPULATION_SIZE*self.SIGMA) * np.dot(A.T, rewards).T
+            
+            #print('population', len(population))
+            #print('rewards', len(rewards))
+            
+            #rewards = (rewards - np.mean(rewards)) / np.std(rewards)
+            
+            
+           
+            
+            
 
